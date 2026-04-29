a82d1d0f54de61ba0414e3a976f70a9e74651d6a
EdwinYHL <126517767+EdwinYHL@users.noreply.github.com>
2026-04-29 15:20:44 -0700

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,15 @@
+// Configuración global
+export const CONFIG = {
+    RECOGNITION_THRESHOLD: 0.55,
+    MODEL_URL: 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights',
+    SERVER_URL: 'http://localhost:5000',
+    ADMIN_USER: 'admin',
+    ADMIN_PASS: '1234',
+    STORAGE_KEYS: {
+        EMPLEADOS: 'empleados',
+        REGISTROS: 'registros',
+        EMOCIONES: 'emocionesHistorial',
+        PUNTOS: 'puntosPositividad',
+        GRUPO_AB: 'grupoAB'
+    }
+};
